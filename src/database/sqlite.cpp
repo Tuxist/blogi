12134d55e230fb098cc06521b16c720dfc562700
jan.koester <jan.koester@tuxist.de>
2024-01-09 11:17:10 +0100

    added forgotten header

--- src/database/sqlite.cpp
+++ src/database/sqlite.cpp
@@ -25,6 +25,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
 
+#include <iostream>
 #include <atomic>
 
 #include <httppp/exception.h>
